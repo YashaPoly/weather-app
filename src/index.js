31a79b18ae5b98fa3e525ec019b0a397aebbca84
YashaPoly <109500555+YashaPoly@users.noreply.github.com>
2022-09-08 22:10:09 +0200

    Forecast data with api

--- src/index.js
+++ src/index.js
@@ -44,6 +44,57 @@ function showDate() {
 }
 let currentMonth = document.querySelector("#current-month");
 currentMonth.innerHTML = showDate();
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="74"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "a6d0f22831811d420a6a12095a318882";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 // Feature #2
 function search(city) {
   let apiKey = "a6d0f22831811d420a6a12095a318882";
@@ -122,6 +173,7 @@ function showTemp(response) {
     response.data.wind.speed * 3.6
   );
   document.querySelector("h1").innerHTML = response.data.name;
+  getForecast(response.data.coord);
 }
 
 function searchCity() {
